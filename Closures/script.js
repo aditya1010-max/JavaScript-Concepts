bc7f3b603132e3a1619dc5d6229d963b2f9a9220
aditya1010-max <rauta2813@gmail.com>
2026-04-07 00:02:21 +0530

    Private methods with closures(imp)

--- Closures/script.js
+++ Closures/script.js
@@ -72,3 +72,37 @@ const add10 = makeAdder1(10);
 
 console.log(add5(2));  //7
 console.log(add10(2)); //12
+
+
+// Private methods with closures
+
+const counter = (function () {
+  let privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+
+  return {
+    increment() {
+      changeBy(1);
+    },
+
+    decrement() {
+      changeBy(-1);
+    },
+
+    value() {
+      return privateCounter;
+    },
+  };
+})();
+
+console.log(counter.value()); // 0.
+
+counter.increment();
+counter.increment();
+console.log(counter.value()); // 2.
+
+counter.decrement();
+console.log(counter.value()); // 1.
+
